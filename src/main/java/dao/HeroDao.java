bb5ebf06e55b8475d844bae76d12db5e5db052ac
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-26 07:04:21 +0300

    Add Data Access Objects for Squad and Hero classes

---
+++ src/main/java/dao/HeroDao.java
@@ -0,0 +1,21 @@
+package dao;
+
+import Models.Hero;
+
+import java.util.List;
+
+public interface HeroDao {
+    //List all Heroes
+    List <Hero> getAllHeroes();
+
+
+    //CRUD
+    void addHero(Hero hero);
+    Hero findById(int id);
+    void UpdateHero(int id,int age,String name,String power,String move,String weapon,String weakness);
+    //Delete one
+    void deleteById(int id);
+
+    //delete all
+    void deleteAll();
+}
---
+++ src/main/java/dao/SquadDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import Models.Hero;
+import Models.Squad;
+
+import java.util.List;
+
+public interface SquadDao {
+
+    //List of all Squads
+    List<Squad> getAll();
+    List <Hero> allHeroesInASquad (int squadId);
+
+    //CRUD
+    void addSquad(Squad squad);
+    Squad findById(int id);
+    void updateSquad(int id,int size,int name,String cause);
+    void deleteAllSquads();
+}
