a2350979783a6e46e3bf1bd50ff3072dd14498c8
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-28 05:15:15 +0300

    Change connection String

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,6 @@ import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,26 +15,23 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        String connectionString = "jdbc:h2:~/hero-squad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString,"","");
         Sql2oHeroDao heroDao = new Sql2oHeroDao(sql2o);
         Sql2oSquadDao squadDao = new Sql2oSquadDao(sql2o);
 
-        //Show Every Hero and Category
+        //Show Every Hero and Squad
         get("/",(request, response) -> {
             Map <String,Object> model = new HashMap<>();
-            List <Hero> heroes = heroDao.getAllHeroes();
-            model.put("heroes",heroes);
-            List <Squad> squads = squadDao.getAllSquads();
-            model.put("squads",squads);
+            model.put("squads",squadDao.getAllSquads());
+            model.put("heroes",heroDao.getAllHeroes());
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
 
         //Get squad Form
         get("/squad/new",(request, response) ->{
             Map <String,Object> model = new HashMap<>();
-            List <Squad> squads = squadDao.getAllSquads();
-            model.put("squads",squads);
+            model.put("squads",squadDao.getAllSquads());
             return new ModelAndView(model,"squad-form.hbs");
         },new HandlebarsTemplateEngine() );
 
@@ -44,7 +40,40 @@ public class App {
         get("/hero/new",(request, response) -> {
             Map <String,Object> model = new HashMap<>();
             List <Hero> heroes = heroDao.getAllHeroes();
+            model.put("heroes",heroes);
+            model.put("squads",squadDao.getAllSquads());//Squads for the select field
             return new ModelAndView(model,"hero-form.hbs");
+        },new HandlebarsTemplateEngine());
+
+
+        //Post Squad Form
+        post("/squads",(request, response) -> {
+//            Map <String,Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            int size = Integer.parseInt(request.queryParams("size"));
+            String cause = request.queryParams("cause");
+            System.out.println(name);
+            System.out.println(size);
+            System.out.println(cause);
+            squadDao.addSquad(new Squad(size,name,cause));
+            System.out.println(squadDao.getAllSquads().size());
+            response.redirect("/");
+            return null;
+        },new HandlebarsTemplateEngine());
+
+        //Post Hero Form
+        post("/heroes",(request, response) -> {
+            Map <String,Object> model = new HashMap<>();
+            String name = request.params("name");
+            int age = Integer.parseInt(request.params("age"));
+            String power = request.params("power");
+            String move = request.params("move");
+            String weapon = request.params("weapon");
+            String weakness = request.params("weakness");
+            int squadId = Integer.parseInt(request.params("squadId"));
+            heroDao.addHero(new Hero(age,name,power,move,weapon,weakness,squadId));
+            response.redirect("/");
+            return null;
         },new HandlebarsTemplateEngine());
 
     }
