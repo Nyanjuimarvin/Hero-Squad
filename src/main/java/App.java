5e58a26c7e6f6834e47a14361e0585fcf903e7bf
Nyanjuimarvin <marnyanjui@gmail.com>
2022-03-01 18:10:40 +0300

    Add changes before heroku deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,17 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         String connectionString = "jdbc:postgresql://localhost:5432/herosquad";
         Sql2o sql2o = new Sql2o(connectionString,"marvin","nrvnqsr13");
